87f3022469068872b33789ad5c6a3cdb8daee1a3
Samuel A. Winful <samuel@winful.com>
2021-09-04 00:17:34 -0400

    relay-topology_pull.sh: Removed need to restart node after update.

--- templates/relay-topology_pull.sh
+++ templates/relay-topology_pull.sh
@@ -7,7 +7,7 @@ CNODE_VALENCY=1
 curl -s -o {{ cnode_conf_dir }}/{{ cluster }}-topology.json "https://api.clio.one/htopology/v1/fetch/?max=${MAX_PEERS}&customPeers=${BLOCKPRODUCING_IP}:${BLOCKPRODUCING_PORT}:${CNODE_VALENCY}|relays-new.cardano-mainnet.iohk.io:3001:2"
 
 # Restart relay after update
-if [ "$?" -eq "0" ]; then
-  sudo {{ prefix }}/etc/rc.d/{{ cnode_type }} restart
-fi
+# if [ "$?" -eq "0" ]; then
+#   sudo {{ prefix }}/etc/rc.d/{{ cnode_type }} restart
+# fi
 
