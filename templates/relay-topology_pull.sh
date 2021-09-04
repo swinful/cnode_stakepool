ca68e26d89801c41652004d93838963897870e2a
Samuel A. Winful <samuel@winful.com>
2021-09-03 22:25:46 -0400

    relay-topology_pull.sh: Added MAX_PEERS variable.

--- templates/relay-topology_pull.sh
+++ templates/relay-topology_pull.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+MAX_PEERS=15
 BLOCKPRODUCING_IP={{ lookup('dig', cnode_producer) }}
 BLOCKPRODUCING_PORT=6000
 CNODE_VALENCY=1
-curl -s -o {{ cnode_conf_dir }}/{{ cluster }}-topology.json "https://api.clio.one/htopology/v1/fetch/?max=20&customPeers=${BLOCKPRODUCING_IP}:${BLOCKPRODUCING_PORT}:${CNODE_VALENCY}|relays-new.cardano-mainnet.iohk.io:3001:2"
+curl -s -o {{ cnode_conf_dir }}/{{ cluster }}-topology.json "https://api.clio.one/htopology/v1/fetch/?max=${MAX_PEERS}&customPeers=${BLOCKPRODUCING_IP}:${BLOCKPRODUCING_PORT}:${CNODE_VALENCY}|relays-new.cardano-mainnet.iohk.io:3001:2"
 
 # Restart relay after update
 if [ "$?" -eq "0" ]; then
